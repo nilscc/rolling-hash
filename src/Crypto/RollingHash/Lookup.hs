886f3aeee3b9fbe8bdd8889c74676fd12f0f5499
Nils Schweinsberg <mail@nils.cc>
2013-02-18 00:16:09 +0100

    use foreign pointers with arrays

--- src/Crypto/RollingHash/Lookup.hs
+++ src/Crypto/RollingHash/Lookup.hs
@@ -17,11 +17,11 @@ import Data.Word
 import Foreign hiding (unsafePerformIO)
 import System.IO.Unsafe
 
-type Array a = Ptr a
+type Array a = ForeignPtr a
 
 data Word32Map a = Word32Map
-  { _word16array :: Array Int     -- ^ positions in lookuplist
-  , _word16len   :: Array Word8   -- ^ number of elements at current word16
+  { _word16array :: Array Int      -- ^ positions in lookuplist
+  , _word16len   :: Array Word16   -- ^ number of elements at current word16
   , _word32array :: Array Word32
   , _elemarray   :: Array a
   }
@@ -43,49 +43,60 @@ fromListW32 l = unsafePerformIO $ do
   let (w32_l,elem_l) = unzip $ sort l
 
   -- poke lists into arrays
-  w32_a  <- newArray w32_l
-  elem_a <- newArray elem_l
+  fw32_a  <- toForeignPtr $ newArray w32_l
+  felem_a <- toForeignPtr $ newArray elem_l
 
   -- create Word16 array
-  w16_a  <- newArray $ replicate (2^(16 :: Int)) 0
-  w16l_a <- newArray $ replicate (2^(16 :: Int)) 0
+  fw16_a  <- toForeignPtr $ newArray $ replicate (2^(16 :: Int)) 0
+  fw16l_a <- toForeignPtr $ newArray $ replicate (2^(16 :: Int)) 0
 
-  forM_ (zip w32_l [0..]) $ \(w32,p) -> do
-    let w16  = takeFst16 w32
-        ptr  = advancePtr w16_a  (w16toInt w16)
-        ptr' = advancePtr w16l_a (w16toInt w16)
-    -- store position
-    p' <- peek ptr
-    when (p' == 0) $
-      poke ptr (p+1)
-    -- increase counter
-    l' <- peek ptr'
-    poke ptr' (l'+1)
+  forM_ (zip w32_l [0..]) $ \(w32,p) -> 
+    withForeignPtr fw16_a $ \ w16_a -> do
+    withForeignPtr fw16l_a $ \w16l_a -> do
+      let w16  = takeFst16 w32
+          ptr  = advancePtr w16_a  (w16toInt w16)
+          ptr' = advancePtr w16l_a (w16toInt w16)
+      -- store position
+      p' <- peek ptr
+      when (p' == 0) $
+        poke ptr (p+1)
+      -- increase counter
+      l' <- peek ptr'
+      poke ptr' (l'+1)
 
-  return $ Word32Map w16_a w16l_a w32_a elem_a
+  return $ Word32Map fw16_a fw16l_a fw32_a felem_a
 
  where
 
-  --toForeignPtr = (>>= newForeignPtr finalizerFree)
+  toForeignPtr = (>>= newForeignPtr finalizerFree)
 
 {-# INLINABLE lookupW32 #-}
 lookupW32 :: Storable a => Word32 -> Word32Map a -> Maybe a
-lookupW32 w32 (Word32Map w16_a w16l_a w32_a elem_a) = unsafePerformIO $ do
-  -- lookup first occurance (if any) of the Word16 part in the array
-  p <- peek (advancePtr w16_a  (w16toInt w16))
-  l <- peek (advancePtr w16l_a (w16toInt w16))
-  if l > 0 then go l (p-1) else return Nothing
+lookupW32 w32 (Word32Map fw16_a fw16l_a fw32_a felem_a) = 
+  unsafePerformIO $
+  withForeignPtr fw16_a $ \w16_a ->
+  withForeignPtr fw16l_a $ \w16l_a -> do
+
+    -- lookup first occurance (if any) of the Word16 part in the array
+    p <- peek (advancePtr w16_a  (w16toInt w16))
+    -- number of entries at current position
+    l <- peek (advancePtr w16l_a (w16toInt w16))
+
+    if l > 0 then go l (p-1) else return Nothing
+
  where
   go 0 _ = return Nothing
   go l p = do
-    -- compare the current value with the wanted one
-    w32' <- peek (advancePtr w32_a p)
-    if w32 == w32' then do
-       -- success
-       el <- peek (advancePtr elem_a p)
-       return $ Just el
-     else
-       -- continue looking
-       go (l-1) (p+1)
+    withForeignPtr fw32_a $ \w32_a -> do
+    withForeignPtr felem_a $ \elem_a -> do
+      -- compare the current value with the wanted one
+      w32' <- peek (advancePtr w32_a p)
+      if w32 == w32' then do
+         -- success
+         el <- peek (advancePtr elem_a p)
+         return $ Just el
+       else
+         -- continue looking at next position
+         go (l-1) (p+1)
 
   w16 = takeFst16 w32
